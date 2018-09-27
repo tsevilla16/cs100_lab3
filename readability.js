b49582164e0328f96d7515b777f311269d4837a3
Doug Lloyd <lloyd@cs50.harvard.edu>
2018-09-27 09:27:46 -0400

    initial commit

---
+++ readability.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const md5File = require('md5-file');
+const sqlite3 = require('sqlite3');
+const Tokenizer = require('tokenize-text');
+const tokenize = new Tokenizer();
+const tokenizeEnglish = require("tokenize-english")(tokenize);
+
+function readability(filename, callback) {
+    fs.readFile(filename, "utf8", (err, contents) => {
+        if (err) throw err;
+
+        // todo: parse and analyze the file
+
+        callback({test: "Replace this object with more information!"});
+    });
+}
+
+// Computes Coleman-Liau readability index
+function colemanLiau(letters, words, sentences) {
+    return (0.0588 * (letters * 100 / words))
+        - (0.296 * (sentences * 100 / words))
+        - 15.8;
+}
+
+// Computes Automated Readability Index
+function automatedReadabilityIndex(letters, numbers, words, sentences) {
+    return (4.71 * ((letters + numbers) / words))
+        + (0.5 * (words / sentences))
+        - 21.43;
+}
+
+readability(process.argv[2], data => {
+    console.log(data);
+});
