10027c03e0e911dac43ba8cd1dad5c5406b50753
Doug Lloyd <lloyd@cs50.harvard.edu>
2018-09-27 09:36:55 -0400

    Update readability.js

--- readability.js
+++ readability.js
@@ -30,6 +30,7 @@ function automatedReadabilityIndex(letters, numbers, words, sentences) {
         - 21.43;
 }
 
+// Calls the readability function on the provided file and defines callback behavior
 readability(process.argv[2], data => {
     console.log(data);
 });
