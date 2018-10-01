5d7c7349fd5c04871f504f6a822bb0393831e718
jcrogoff <jcrogoff@gmail.com>
2018-09-30 21:01:32 -0400

    adding final version of project with comments

--- readability.js
+++ readability.js
@@ -19,9 +19,11 @@ function readability(filename, callback) {
     fs.readFile(filename, "utf8", (err, contents) => {
         if (err) throw err;
 
+        //create variables to store coleman liau and automated readability
         let colemanLiauText = 0;
         let automatedReadabilityIndexText = 0;
 
+        //generate hash based on given filename
         const fileHash = md5File.sync(filename);
 
         //check if row exists in database table
@@ -30,7 +32,7 @@ function readability(filename, callback) {
               if (err) {
                   throw err;
               }
-
+                //if the file information is not storred in the database
                 if (row == undefined){
                     
                     //parse and analyze the file contents
@@ -42,6 +44,7 @@ function readability(filename, callback) {
                     const nonewlines = contents.split(/\n/).join(' ');
                     const sentencesTokens = tokenizeEnglish.sentences()(nonewlines)
 
+                    //calculate coleman liau score and automated readability index score
                     colemanLiauText = colemanLiau(lettertokens.length, wordtokens.length, sentencesTokens.length)
                     automatedReadabilityIndexText = automatedReadabilityIndex(lettertokens.length, numberTokens.length, wordtokens.length, sentencesTokens.length)
 
@@ -49,6 +52,7 @@ function readability(filename, callback) {
                     //insert row into database
                     insertDBRow (fileHash, numberTokens.length, lettertokens.length, wordtokens.length, sentencesTokens.length, colemanLiauText, automatedReadabilityIndexText)
 
+                    //callback the calculated file information
                     callback(
                         "characters: " + (numberTokens.length + lettertokens.length) + "\n" +
                         "words: " + wordtokens.length + "\n" +
@@ -59,7 +63,9 @@ function readability(filename, callback) {
                     );
 
                 }
+                //else if the file information is stored in the database
                 else {
+                        //callback the file information in the database
                         callback(
                             "characters: " + (row.numbercount + row.lettercount) + "\n" +
                             "words: " + row.wordcount + "\n" +
@@ -103,7 +109,7 @@ else {
 //copied and modified code from http://www.sqlitetutorial.net/sqlite-nodejs/insert/
 // insert one row into the textsinfo table
 function insertDBRow (fileHash, numberscount, letterscount, wordscount, sentencescount, colemanLiauNum, automatedReadabiltyNum){
-    db.run(`INSERT INTO textsinfo(fileHash, numbercount, lettercount, wordcount, sentencescount, colemanLiau, automatedReadabilty) VALUES(?,?,?,?,?,?,?)`, [fileHash, numberscount, letterscount, wordscount, sentencescount, colemanLiauNum, automatedReadabiltyNum], function(err) {
+    db.run(`INSERT INTO textsinfo(fileHash, numbercount, lettercount, wordcount, sentencescount, colemanLiau, automatedReadability) VALUES(?,?,?,?,?,?,?)`, [fileHash, numberscount, letterscount, wordscount, sentencescount, colemanLiauNum, automatedReadabiltyNum], function(err) {
         if (err) {
         return console.log(err.message);
         }
